squizzeak/openpilot

Allow lead chevron, developer UI, and longitudinal metrics without longitudinal control

selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -686,7 +686,7 @@ void AnnotatedCameraWidget::paintEvent(QPaintEvent *event) {
     update_model(s, model, sm["uiPlan"].getUiPlan());
     drawLaneLines(painter, s);
 
-    if (s->scene.longitudinal_control && sm.rcv_frame("modelV2") > s->scene.started_frame && !s->scene.hide_lead_marker) {
+    if (sm.rcv_frame("modelV2") > s->scene.started_frame && !s->scene.hide_lead_marker) {
       update_leads(s, model);
       float prev_drel = -1;
       for (int i = 0; i < model.getLeadsV3().size() && i < 2; i++) {

squizzeak/FrogPilot

selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -855,7 +855,7 @@ void AnnotatedCameraWidget::paintEvent(QPaintEvent *event) {
     update_model(s, model, sm["uiPlan"].getUiPlan());
     drawLaneLines(painter, s, v_ego);
 
-    if (s->scene.longitudinal_control && sm.rcv_frame("radarState") > s->scene.started_frame && !s->scene.hide_lead_marker) {
+    if (sm.rcv_frame("radarState") > s->scene.started_frame && !s->scene.hide_lead_marker) {
       auto radar_state = sm["radarState"].getRadarState();
       update_leads(s, radar_state, model.getPosition());
       auto lead_one = radar_state.getLeadOne();